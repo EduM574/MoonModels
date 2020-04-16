7de74939b5d303e870b80b25953badb46986ff2d
kesselyn <kesselynfortunato@hotmail.com>
2020-04-16 00:35:49 -0300

    Servlet Assistant

--- src/controller/AssistantController.java
+++ src/controller/AssistantController.java
@@ -7,16 +7,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 import com.google.gson.Gson;
-// import com.ibm.cloud.sdk.core.service.security.IamOptions;
-// import com.ibm.watson.assistant.v1.model.Context;
-// import com.ibm.watson.assistant.v1.model.MessageInput;
-// import com.ibm.watson.assistant.v1.model.MessageOptions;
+import com.ibm.cloud.sdk.core.security.Authenticator;
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.cloud.sdk.core.service.security.IamOptions;
+import com.ibm.watson.assistant.v1.model.Context;
+import com.ibm.watson.assistant.v1.model.MessageInput;
+import com.ibm.watson.assistant.v1.model.MessageOptions;
 import com.ibm.watson.assistant.v1.model.MessageResponse;
-// import com.ibm.watson.assistant.v1.Assistant;
+import com.ibm.watson.assistant.v1.Assistant;
 
 @WebServlet("/assitant")
 public class AssistantController extends HttpServlet{
-    // private Context context = null;
+    private Context context = null;
 	private static final long serialVersionUID = 1L;
 	
 	/**
@@ -34,35 +36,33 @@ public class AssistantController extends HttpServlet{
     }
 
     private MessageResponse assistantAPICall(String msg) {
-        
+
         //Configuracao de autenticao do servico
         //Colocar a sua APIKEY
-        // IamOptions options = new IamOptions.Builder().apiKey("0t2zas9GM4EHcgyVdUx1ZycrpZzzdouCAGxVMGEKcA3q").build();
-        
+        Authenticator authenticator = new IamAuthenticator("N1Wy_LvlBViEMjNuWWfqD_ljerPcpCIJniXNfShsaMKp");
         // Criando o objeto do servico desejado
-        // Assistant service = new Assistant("2018-02-16", options);
+        Assistant service = new Assistant("2020-04-07", authenticator);
+
 		//Colocar a sua WORKSPACEID
-        // String workspaceId = "b477e72c-8e53-499e-a177-4a95559df609";
+        String workspaceId = "d762afb2-67d3-43f2-87c4-8d1d6c37050b";
         
         // Preparando a mensagem de envio
-        // MessageInput input = new MessageInput();
-        // input.setText(msg);
+        MessageInput input = new MessageInput();
+        input.setText(msg);
         
         // Configurando os parametros para o Watson
-        // MessageOptions messageOptions = new MessageOptions.Builder()
-		// 		.workspaceId(workspaceId)
-		// 		.input(input)
-		// 		.context(this.context)
-        // 		.build();
+        MessageOptions messageOptions = new 
+            MessageOptions.Builder()
+            .workspaceId(workspaceId)
+            .input(input)
+            .context(this.context)
+            .build();
         
         // Conectando com o Assistant e recebendo a resposta dele
-        // MessageResponse response  = service.message(messageOptions)
-		// 		.execute()
-		// 		.getResult();
+        MessageResponse response = service.message(messageOptions).execute().getResult();
 		
-		// this.context = response.getContext();
+		this.context = response.getContext();
 
-		// return response;
-        return null;
+		return response;
     }
 }
