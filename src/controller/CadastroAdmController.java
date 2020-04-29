d672f869ff48410ebc3f75298a52fb0ee41b3c20
kesselyn <kesselynfortunato@hotmail.com>
2020-04-29 12:40:36 -0300

    Arrumando a servlet de create adm

--- src/controller/CadastroAdmController.java
+++ src/controller/CadastroAdmController.java
@@ -1,7 +1,7 @@
 package controller;
 
 import java.io.IOException;
-// import java.io.PrintWriter;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,8 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.Administrador;
 import model.Setor;
-// import service.AdministradorService;
-// import model.Validation;
+import service.AdministradorService;
+import model.Validation;
 
 /**
  * Servlet implementation class CadastroAdmController
@@ -33,12 +33,11 @@ public class CadastroAdmController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String pSetor = request.getParameter("setor");
 		String pNome = request.getParameter("nome");
 		String pSobrenome = request.getParameter("sobrenome");
-		String pCpf = request.getParameter("cpf");
 		String pEmail = request.getParameter("email");
-		String pSenha =  request.getParameter("password");
+		String pCpf = request.getParameter("cpf");
+		String pSetor = request.getParameter("setor");
 
 		int setConv = Integer.parseInt(pSetor);
 		Setor setor = new Setor();
@@ -47,33 +46,31 @@ public class CadastroAdmController extends HttpServlet {
 		Administrador adm = new Administrador();
 		adm.setNome(pNome);
 		adm.setSobrenome(pSobrenome);
-		adm.setCpf(pCpf);
 		adm.setEmail(pEmail);
-		adm.setSenha(pSenha);
+		adm.setCpf(pCpf);
 		adm.setSetor(setor);
 		
-		// AdministradorService as = new AdministradorService();
-		// Validation v = as.updateInicialValidation(adm);
+		AdministradorService as = new AdministradorService();
+		Validation v = as.createValidation(adm);
 		
-		// if(v.getStatus()) {
-		// 	PrintWriter out = response.getWriter();
-		// 	out.println("<html><head><title>Cadastro Administrador</title></head><body>");
-		// 	out.println(	"Erro = " + v.getText() + "<br>");			
-		// 	out.println("</body></html>");
-		// } else {
+		if(v.getStatus()) {
+			PrintWriter out = response.getWriter();
+			out.println("<html><head><title>Cadastro Administrador</title></head><body>");
+			out.println(	"Erro = " + v.getText() + "<br>");			
+			out.println("</body></html>");
+		} else {
+			as.create(adm);
+			adm = as.selectAdminGeral(adm);
 		
-		// 	as.updateInicial(adm);
-		// 	adm = as.selectAdminGeral(adm);
-		
-		// 	PrintWriter out = response.getWriter();
-		// 	out.println("<html><head><title>Cadastro Administrador</title></head><body>");
-		// 	out.println(	"Nome = " + adm.getNome() + "<br>");
-		// 	out.println(	"Sobrenome = " + adm.getSobrenome() + "<br>");		
-		// 	out.println(	"Cpf = " + adm.getCpf() + "<br>");
-		// 	out.println(	"Email = " + adm.getEmail() + "<br>");
-		// 	out.println(	"Senha = " + adm.getSenha() + "<br>");
-		// 	out.println("</body></html>");
-		// }
+			PrintWriter out = response.getWriter();
+			out.println("<html><head><title>Cadastro Administrador</title></head><body>");
+			out.println(	"Nome = " + adm.getNome() + "<br>");
+			out.println(	"Sobrenome = " + adm.getSobrenome() + "<br>");		
+			out.println(	"Cpf = " + adm.getCpf() + "<br>");
+			out.println(	"Email = " + adm.getEmail() + "<br>");
+			out.println(	"Senha = " + adm.getSenha() + "<br>");
+			out.println("</body></html>");
+		}
 	}
 
 }
