421241577162404774f8488edc5ff0ddd1aed391
kesselyn <kesselynfortunato@hotmail.com>
2020-05-04 17:15:12 -0300

    clear

--- src/controller/CadastroSolicitacoesController.java
+++ src/controller/CadastroSolicitacoesController.java
@@ -1,18 +1,14 @@
 package controller;
 
-// import java.io.File;
 import java.io.IOException;
-// import java.io.InputStream;
 import java.io.PrintWriter;
-// import java.nio.file.Paths;
-// import java.io.FileOutputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-// import javax.servlet.http.Part;
+import javax.servlet.http.HttpSession;
 
 import model.Aluno;
 import model.Solicitacao;
@@ -40,60 +36,23 @@ public class CadastroSolicitacoesController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// atributos capturados
+
 		String sNome = request.getParameter("solicitacao");
 		String sDescricao = request.getParameter("descricao");
+
+		HttpSession session = request.getSession();
+		Aluno al = (Aluno) session.getAttribute("aluno");
+
+		Solicitacao sol = new Solicitacao();
+		sol.setNome(sNome);
+		sol.setDescricao(sDescricao);
+		sol.setAluno(al);
+		// sol.setAnexo(file);
+
+		SolicitacaoService ss = new SolicitacaoService();
+		ss.create(sol);
+
 		
-		// Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
-//		String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
-		// InputStream fileContent = filePart.getInputStream();
-		
-		// String FILE_TO = "c:\\Download\\google.pdf";
-		
-		// try {
-            // File file = new File(FILE_TO);
-            // copyInputStreamToFile(fileContent, file);
-            
-            // criar obj
-            Aluno al = new Aluno();
-            al.setRa(4);// id pronto, trocar depois
-            Solicitacao sol = new Solicitacao();
-            sol.setNome(sNome);
-            sol.setDescricao(sDescricao);
-            sol.setAluno(al);
-            // sol.setAnexo(file);
-            
-            SolicitacaoService ss = new SolicitacaoService();
-            ss.create(sol);
-            
-            PrintWriter out = response.getWriter();
-            out.println("<html><head><title>Cadastro Administrador</title></head><body>");
-            out.println("Nome da Solicitação = " + sol.getNome() + "<br>");
-            out.println("Descrição = " + sol.getDescricao() + "<br>");
-            out.println("Ra = " + sol.getAluno().getRa() + "<br>");
-            out.println("</body></html>");
-        // } catch(IOException e) {
-        // 	e.printStackTrace();
-        // }
 
 	}
-
-	// private static void copyInputStreamToFile(InputStream inputStream, File file)
-	// 		throws IOException {
-
-	//         try (FileOutputStream outputStream = new FileOutputStream(file)) {
-
-	//             int read;
-	//             byte[] bytes = new byte[1024];
-
-	//             while ((read = inputStream.read(bytes)) != -1) {
-	//                 outputStream.write(bytes, 0, read);
-	//             }
-
-	// 			// commons-io
-	//             //IOUtils.copy(inputStream, outputStream);
-
-	//         }
-
-	//     }
 }
