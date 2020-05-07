0bac24b887ec01fc4716f253e67b3a9f320e6619
Eduardo Moraes <eduardomoraeslima@hotmail.com>
2020-05-07 10:49:17 -0300

    editar adm

--- src/controller/EditarAdmDadosController.java
+++ src/controller/EditarAdmDadosController.java
@@ -9,15 +9,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import model.Administrador;
 import model.Aluno;
+import model.Setor;
+import service.AdministradorService;
 import service.AlunoService;
 
 /**
  * Servlet implementation class EditarAlunoDadosController
  */
-@WebServlet("/EditarAlunoDados.do")
-public class EditarAlunoDadosController extends HttpServlet {
+@WebServlet("/EditarAdmDados.do")
+public class EditarAdmDadosController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -31,47 +35,37 @@ public class EditarAlunoDadosController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		 Administrador ad =(Administrador) session.getAttribute("adm");
 		
 		String pNome = request.getParameter("nome");
-		String pSobrenome = request.getParameter("sobrenome");
 		String pCpf = request.getParameter("cpf");
-		String pEmail = request.getParameter("email");
+		String pSetor = request.getParameter("setor");
+		String pSobrenome = request.getParameter("sobrenome");
 		String pSenha = request.getParameter("password");
-		String pDataNascimento = request.getParameter("nascimento");
-		String pCurso = request.getParameter("curso");
-		String pSemestre = request.getParameter("semestre");
-		String pUnidade = request.getParameter("unidade");
-		String pTurno = request.getParameter("turno");
 		String pStatus = request.getParameter("status");
+
+		Setor st = new Setor();
+		if(pSetor.equals("GDE")) {
+			st.setIdSetor(2);
+		}else if(pSetor.equals("TPE")) {
+			st.setIdSetor(1);
+		}else {
+			st.setIdSetor(3);
+		}
+		Administrador adm = new Administrador();
+		adm.setNome(pNome);
+		adm.setSobrenome(pSobrenome);
+		adm.setCpf(pCpf);
+		adm.setSenha(pSenha);
+		adm.setSetor(st);
+		adm.setStatus(pStatus);
+		adm.setEmail(ad.getEmail());
 		
-		String[] dataSeparada = pDataNascimento.split("-");
-		int ano = Integer.parseInt(dataSeparada[0]);
-		int mes = Integer.parseInt(dataSeparada[1]);
-		int dia = Integer.parseInt(dataSeparada[2]);
+		AdministradorService as = new AdministradorService();
+		as.updateTotal(adm);
 		
-		int sem = Integer.parseInt(pSemestre);
-		
-		GregorianCalendar cal = new GregorianCalendar(ano, mes, dia);
-		
-		Aluno aln = new Aluno();
-		aln.setNome(pNome);
-		aln.setSobrenome(pSobrenome);
-		aln.setCpf(pCpf);
-		aln.setEmail(pEmail);
-		aln.setSenha(pSenha);
-		aln.setData_nascimento(cal);
-		aln.setCurso(pCurso);
-		aln.setSemestre(sem);
-		aln.setUnidade(pUnidade);
-		aln.setTurno(pTurno);
-		aln.setStatus(pStatus);
-		//RA e ADM para testes
-		aln.setRa(819100000);
-		
-		AlunoService as = new AlunoService();
-		as.updateDoAdm(aln);
-		
-		String title = "Dados do aluno alterados com sucesso.";
+		String title = "Dados do administrador alterados com sucesso.";
 		String data = "";
 		request.setAttribute("title", title);
 		request.setAttribute("data", data);
