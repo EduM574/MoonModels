5f9831ad6a4e8dbb66c2578dd377fd7a3477769b
Kesselyn Fortunato <52460720+Kesselyn@users.noreply.github.com>
2020-08-15 22:31:00 -0300

    Create ConnectionFactory.java

---
+++ src/dao/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.sql.*;
+
+public class ConnectionFactory {
+	public static Connection conectar() {
+    String user = "";
+    String password = "";
+    
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			return DriverManager.getConnection("jdbc:mysql://localhost/moon?useTimezone=true&serverTimezone=America/Sao_Paulo&useSSL=false", user, password);
+		} catch(Exception e) {
+			System.err.println("Falha: " + e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
