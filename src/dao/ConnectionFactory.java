ce5c0109e46ad954a08d05c2a0d9177ae50a68b3
Kesselyn Fortunato <52460720+Kesselyn@users.noreply.github.com>
2020-08-15 22:32:20 -0300

    Update ConnectionFactory.java

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -4,9 +4,9 @@ import java.sql.*;
 
 public class ConnectionFactory {
 	public static Connection conectar() {
-    String user = "";
-    String password = "";
-    
+		String user = "";
+		String password = "";
+
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			return DriverManager.getConnection("jdbc:mysql://localhost/moon?useTimezone=true&serverTimezone=America/Sao_Paulo&useSSL=false", user, password);
